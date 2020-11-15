c7233a6fa65665d4ae9abc21586a07b8a6c3863d
lucianohorvath <lucianohorvath94@gmail.com>
2020-11-15 20:52:58 -0300

    fix login and register (mocked)

--- ArtMarket.UI.Web/Controllers/AccountController.cs
+++ ArtMarket.UI.Web/Controllers/AccountController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Linq;
+using System.Data.Entity.Infrastructure;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
-using ArtMarket.Business;
 using ArtMarket.Entities.Model;
 using ArtMarket.UI.Web.Models;
 
@@ -13,61 +11,105 @@ namespace ArtMarket.UI.Web.Controllers
 	[Authorize]
 	public class AccountController : Controller
 	{
-        //
-        // GET: /Account/Login
-        [AllowAnonymous]
-        public ActionResult Login(string returnUrl)
-        {
-            ViewBag.ReturnUrl = returnUrl;
+		//
+		// GET: /Account/Login
+		[AllowAnonymous]
+		public ActionResult Login(string returnUrl)
+		{
+			ViewBag.ReturnUrl = returnUrl;
 
-            return View(new LoginViewModel());
+			return View(new LoginViewModel());
+		}
+
+		//
+		// POST: /Account/Login
+		[HttpPost]
+		[AllowAnonymous]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			// TODO: pegarle a la API para intentar loguearse
+            if (model.Password == "Hola1!")
+            {
+                FormsAuthentication.SetAuthCookie(model.Email, false);
+
+                return RedirectToAction("Index", "Home");
+			}
+            else
+            {
+                ModelState.AddModelError("", "Inicio de sesión fallido: credenciales inválidas.");
+
+                return View(model);
+			}
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Login(User user)
-        {
-            try
-            {
-                //TODO Coneccion a a BD
-                /*
-                  if (db.SEC_User.Where(x => x.UserName == user.UserName && x.IsActive).Any())
-                  {
-                      StringCypher sc = new StringCypher();
-                      var realUser = db.SEC_User.Where(x => x.UserName == user.UserName && x.IsActive).First();
+		//
+		// GET: /Account/Register
+		[AllowAnonymous]
+		public ActionResult Register()
+		{
+			return View(new RegisterViewModel());
+		}
 
-
-                      var psw = sc.DesEncriptar(realUser.Password);
-                      if (psw == user.Password)
-                      {
-                          FormsAuthentication.SetAuthCookie(user.UserName, false);
-                          return RedirectToAction("Index", "Home");
-                      }
-                      sc = null;
-                  }
-                  */
-                var psw = "Metodo Que va a la bd a buscar el password de usuario";
-                if (psw == user.Password)
+		//
+		// POST: /Account/Register
+		[HttpPost]
+		[AllowAnonymous]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Register(RegisterViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				if (model.Password != model.ConfirmPassword)
                 {
-                    FormsAuthentication.SetAuthCookie(user.UserName, false);
-                    return RedirectToAction("Index", "Home");
+                    ModelState.AddModelError("", "Las contraseñas no coinciden.");
+                    return View(model);
                 }
 
-                ViewBag.ErrorMessage = "Los datos ingresados no son correctos";
-                return View(user);
-            }
-            catch (Exception ex)
-            {
-                ViewBag.ErrorMessage = "Los datos ingresados no son correctos, Error.";
-                return View(user);
-            }
-        }
+				var user = new User();
+                user.Email = model.Email;
+                user.Password = model.Password;
+                user.Country = "Argentina";
+                user.OrderCount = 0;
+				user.SignUpDate = DateTime.Now;
 
-        [Authorize]
-        public ActionResult Logout()
-        {
+				//
+				// TODO: Intentar registrar pegándole a la API. Manejar errores.
+				//
+
+                return RedirectToAction("Index", "Home");
+			}
+
+			// Algo falló, redibujamos formulario
+			return View(model);
+		}
+
+		//
+		// POST: /Account/LogOff
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult LogOff()
+		{
             FormsAuthentication.SignOut();
+
             return RedirectToAction("Index", "Home");
-        }
+		}
+
+		public ActionResult LoginLogout()
+		{
+			if (User.Identity.IsAuthenticated)
+			{
+                FormsAuthentication.SignOut();
+
+                return RedirectToAction("Index", "Home");
+			}
+
+			return RedirectToAction("Login");
+		}
     }
 }
