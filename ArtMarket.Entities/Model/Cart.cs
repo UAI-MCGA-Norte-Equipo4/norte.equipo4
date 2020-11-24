297b54f9098ab2c4e4681eab506a9ab956445a80
lucianohorvath <lucianohorvath94@gmail.com>
2020-11-23 21:31:16 -0300

    add Cart endpoints

--- ArtMarket.Entities/Model/Cart.cs
+++ ArtMarket.Entities/Model/Cart.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace ArtMarket.Entities.Model
 {
     [Serializable]
+    [DataContract]
     public partial class Cart : IdentityBase
     {
         public Cart()
@@ -11,8 +15,16 @@ namespace ArtMarket.Entities.Model
             this.CartItem = new HashSet<CartItem>();
         }
 
+        [DataMember]
+        [DisplayName("Cookie")]
         public string Cookie { get; set; }
+
+        [DataMember]
+        [DisplayName("Fecha de creación")]
         public DateTime CartDate { get; set; }
+
+        [DataMember]
+        [DisplayName("Cantidad de ítems")]
         public int ItemCount { get; set; }
 
         public virtual ICollection<CartItem> CartItem { get; set; }
