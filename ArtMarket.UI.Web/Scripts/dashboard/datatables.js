6a18999b7a11e9fa9bdf5624d8f1a30db9764464
lucianohorvath <lucianohorvath94@gmail.com>
2020-12-20 01:08:55 -0300

    add image to order detail

--- ArtMarket.UI.Web/Scripts/dashboard/datatables.js
+++ ArtMarket.UI.Web/Scripts/dashboard/datatables.js
@@ -5,11 +5,12 @@ $(document).ready(function() {
             language: {
                 url: '/Scripts/dashboard/localization/Spanish.json'
             },
-            "order": [[1, "desc"]], // Orden por defecto
+            "order": [[0, "desc"]], // Orden por defecto
             "columnDefs": [
                 { "type": "date", "targets": 1 }, // Asigno tipo Date a la columna de fecha
                 { orderable: false, "targets": 5 }  // Desactivar ordenado en columna con índice 5
-            ]
+            ],
+            "lengthMenu": [5, 10, 25, 50]
         }
     );
 });
@@ -19,9 +20,11 @@ $(document).on("click", ".open-detail", function () {
     var productDesc = $(this).data('product-desc');
     var artistName = $(this).data('artist-name');
     var artistDesc = $(this).data('artist-desc');
+    var productImage = $(this).data('product-image');
 
     $(".modal-body #productName").text("Nombre del producto: " + productName);
     $(".modal-body #productDesc").text("Descripción del producto: " + productDesc);
     $(".modal-body #artistName").text("Artista: " + artistName);
     $(".modal-body #artistDesc").text("Descripción del artista: " + artistDesc);
+    $(".modal-body #productImage").attr("src", "/Content/img/product/" + productImage);
 });
