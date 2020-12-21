5d7a22ddad4144cb1a03210f831e32212f8e74a1
lucianohorvath <lucianohorvath94@gmail.com>
2020-12-21 16:02:13 -0300

    bug fix

--- ArtMarket.Services.Http/UserService.cs
+++ ArtMarket.Services.Http/UserService.cs
@@ -16,7 +16,7 @@ namespace ArtMarket.Services.Http
         [Route("Login")]
         public User Login(string email, string password)
         {
-            User user = new User({Email = email};
+            User user = new User {Email = email};
             try
             {
                 //var us = new UserBusiness();
